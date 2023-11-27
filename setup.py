9b80dcd4f3682200f8226d74ac30efc7e977ecc4
Arnaud Odet <arnaud.odet.pro@gmail.com>
2023-11-27 11:25:07 +0100

    packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content]
 
-setup(name='swiss_round_pack',
+setup(name='swiss_round_simulator',
       description="Swiss Round Package",
       packages=find_packages(),
       install_requires=requirements)
